abae7494cbcef72e843413b312a4fb5442bc9941
juncwang <juncwang@live.com>
2019-05-15 10:41:10 +0800

    feat: 新增 Pug Ejs 模版演示

--- server/index.js
+++ server/index.js
@@ -1,11 +1,27 @@
 const Koa = require('koa')
 const app = new Koa()
 
-const { normal } = require('./tpl/index')
+// 引入文件夹下的文件, 不明确写出文件名时, 默认为引用 index.js
+const { htmlTpl, ejsTpl, pugTpl} = require('./tpl')
+// ejs 模版引擎, npm install ejs
+// const ejs = require('ejs')
+// pug 模版引擎, npm install pug
+const pug = require('pug')
 
 app.use(async (ctx, next) => {
     ctx.type = 'text/html; charset=utf-8'
-    ctx.body = normal
+    // 使用 html 文件渲染页面
+    // ctx.body = htmlTpl
+    // 使用 ejs 模版引擎渲染页面, 后面为传入的参数
+    // ctx.body = ejs.render(ejsTpl, {
+    //     you: 'Luke',
+    //     me: 'Scott'
+    // })
+    // 使用 pug 模版引擎渲染页面, 后面为传入的参数
+    ctx.body = pug.render(pugTpl, {
+        you: 'Luke',
+        me: 'Scott'
+    })
 })
 
 app.listen(5000)
