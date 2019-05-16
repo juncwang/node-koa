8e43ee4452bc299603894fbf71b7de3ed3f5f0d0
juncwang <juncwang@live.com>
2019-05-16 13:19:05 +0800

    feat: child_process 增加子进程跑爬虫脚本

--- server/tasks/movie.js
+++ server/tasks/movie.js
@@ -2,10 +2,11 @@ const cp = require('child_process')
 const {resolve } = require('path')
 
 ;(async () => {
-    const script = resolve(__dirname, '../crawler/trailer-list')
-    const child = cp.fork(script, [])
-    let invoked = false
+    const script = resolve(__dirname, '../crawler/trailer-list') //子进程的脚本
+    const child = cp.fork(script, []) //创建这个子进程
+    let invoked = false //子进程的标识符
 
+    //错误的处理
     child.on('error', err => {
         if(invoked) return 
 
@@ -13,13 +14,16 @@ const {resolve } = require('path')
         console.log(err)
     })
 
+    //子进程退出返回码
     child.on('exit', code => {
         if(invoked) return 
 
         invoked = true
         let err = code === 0 ? null : new Error('exit code ' + code)
+        console.log(err)
     })
 
+    //监听message消息，得到data，也即子进程返回的结果
     child.on('message', data => {
         let result = data.result
 
